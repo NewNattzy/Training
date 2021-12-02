9c1a210557088c7b3c28014748c871092f61cdfe
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-02 12:50:59 +0300

    Create Task 3: for.cs
    Залил easy задание по циклам

---
+++ C#/Lesson 1/Task 3: for.cs
@@ -0,0 +1,28 @@
+/*
+    За каждый месяц банк начисляет к сумме вклада 7% от суммы.
+    Напишите консольную программу, в которую пользователь вводит сумму вклада и количество месяцев.
+    А банк вычисляет конечную сумму вклада с учетом начисления процентов за каждый месяц.
+*/
+
+
+using System;
+
+namespace ProcentCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сумму вклада: ");
+            decimal Contribution = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Введите кол-во месяцве: ");
+            int Month = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < Month; i++)
+            {
+                Contribution += Contribution * 0.07M;
+                Console.WriteLine($"Ваш вклад за {i+1} месяц составляет: {Contribution}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
