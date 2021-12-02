1487c09ab428484d70cfb09b13d20f7802186911
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-02 17:07:02 +0300

    Create Program.cs
    Залил задание по инверсии массива

---
+++ C#/Lesson 1/Task 6: array inversion/Program.cs
@@ -0,0 +1,30 @@
+/*
+    Задан следующий массив { -n ... -4, -3, -2, -1, 0, 1, 2, 3, 4 ... n }
+    Необходимо инвертировать данный массив в {n ... 4, 3, 2, 1, 0, -1, -2, -3, -4 ... -n }
+*/
+
+using System;
+
+namespace ArraySort
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = { -10, -8, -4, -3, -2, -1, 0, 1, 2, 3, 4, 10, 30, 50};
+            int[] Result = new int[Numbers.Length];
+            int j = Numbers.Length-1;
+            foreach (int i in Numbers)
+            {
+                Result[j] = i;
+                j--;
+            }
+            Console.Write("{ ");
+            foreach (int i in Result)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.Write("}");
+        }
+    }
+}
