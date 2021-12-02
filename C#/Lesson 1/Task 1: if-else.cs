7dee17092cb85215cbd15ab38929ebc81380a9c1
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-02 10:17:43 +0300

    Create Task 1: if-else.cs
    Залил первое задание по условным конструкциям

---
+++ C#/Lesson 1/Task 1: if-else.cs
@@ -0,0 +1,30 @@
+/*
+    В банке в зависимости от суммы вклада начисляемый процент по вкладу может отличаться.
+    Напишите консольную программу, в которую пользователь вводит сумму вклада.
+    Если сумма вклада меньше 100, то начисляется 5%. Если сумма вклада от 100 до 200, то начисляется 7%.
+    Если сумма вклада больше 200, то начисляется 10%. В конце программа должна выводить сумму вклада с начисленными процентами.
+*/
+
+using System;
+
+namespace PercentCalc
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада");
+            double Money = Convert.ToDouble(Console.ReadLine());
+            double Percent;
+            if (Money < 100)
+                Percent = Money / 100 * 5;
+            else if (Money >= 100 && Money <= 200)
+                Percent = Money / 100 * 7;
+            else
+                Percent = Money / 100 * 10;
+            Money += Percent;
+            Console.WriteLine($"Ваш счет {Money}");
+            Console.ReadKey();
+        }
+    }
+}
