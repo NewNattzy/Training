0de992c95c2ce44e336f03bd3cada625074dc4e6
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-09 17:45:05 +0300

    Update Program.cs
    Переделал main под текущий генератор персонажа. Криво, но работает. Восторг.

--- C#/Game/Program.cs
+++ C#/Game/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using Character;
+using Creature;
 
 namespace ConsoleGame
 { 
@@ -25,7 +25,7 @@ namespace ConsoleGame
             while (specialchoice == false)
             {
                 Console.WriteLine($"Оракул: Отлично, {playername}. Теперь выбери свою судьбу. Ты Воин, Маг или Лучник?");
-                Console.Write($" {playername}:");
+                Console.Write($"{playername}: ");
                 playerspecial = Console.ReadLine();
                 foreach (string i in special)
                 {
@@ -38,8 +38,41 @@ namespace ConsoleGame
                 }
             }
 
+            int basehealth = 0;
+            int basemana = 0;
+            int gold = 0;
+
+            switch (playerspecial)
+            {
+                case "Воин":
+                    basehealth = 200;
+                    basemana = 50;
+                    gold = 0;
+                    break;
+                case "Маг":
+                    basehealth = 75;
+                    basemana = 200;
+                    gold = 50;
+                    break;
+                case "Лучник":
+                    basehealth = 150;
+                    basemana = 100;
+                    gold = 150;
+                    break;
+            }
+
+            // Создаем игрока, проверяем корректность статов
+            Player player = new Player(playername, basehealth, basemana, 10, 1, gold, playerspecial);
+            Console.WriteLine("Характеристики персонажа");
+            Console.WriteLine($"Имя героя: {player.name}");
+            Console.WriteLine($"Класс: {player.special}");
+            Console.WriteLine($"Уровень: {player.Level}");
+            Console.WriteLine($"Здоровье: {player.Health}");
+            Console.WriteLine($"Мана: {player.Mana}");
+            Console.WriteLine($"Золото: {player.Gold}");
+            Console.WriteLine($"Опыт: {player.Exp}");
             Console.WriteLine("Оракул: Твои преключения начинаются!");
-            Player player = new Player(playername, playerspecial);
+            Console.ReadKey();
         }
     }
 }
