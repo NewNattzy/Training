4777da3512b90c4327dc7926eb57957d66e5eb2f
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-10 13:43:11 +0300

    Вынес генерацию персонажа отдельно

--- C#/Game/Program.cs
+++ C#/Game/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using Creature;
+using Generator;
 
 namespace ConsoleGame
 { 
@@ -7,67 +8,15 @@ namespace ConsoleGame
     {
         static void Main(string[] args)
         {
-            string? playername = "";
-            while (true)
-            { 
-                Console.WriteLine("Оракул: Введи свое имя.");
-                Console.Write("Ты: ");
-                playername = Console.ReadLine();
-                Console.Clear();
-                if (playername != "" && playername.Length >= 3)
-                    break;
-            }
-            
-            string[] special = { "Воин", "Маг", "Лучник" };
-            string? playerspecial = "";
-            bool specialchoice = false;
-            Thread.Sleep(250);
-            while (specialchoice == false)
-            {
-                Console.WriteLine($"Оракул: Отлично, {playername}. Теперь выбери свою судьбу. Ты Воин, Маг или Лучник?");
-                Console.Write($"{playername}: ");
-                playerspecial = Console.ReadLine();
-                foreach (string i in special)
-                {
-                    if (i == playerspecial)
-                        specialchoice = true;
-                    Console.Clear();
-                }
-            }
-
-            int basehealth = 0;
-            int basemana = 0;
-            int gold = 0;
-            Thread.Sleep(250);
-            switch (playerspecial)
-            {
-                case "Воин":
-                    basehealth = 200;
-                    basemana = 50;
-                    gold = 0;
-                    break;
-                case "Маг":
-                    basehealth = 75;
-                    basemana = 200;
-                    gold = 50;
-                    break;
-                case "Лучник":
-                    basehealth = 150;
-                    basemana = 100;
-                    gold = 150;
-                    break;
-            }
-
-            // Создаем игрока, проверяем корректность статов
-            Player player = new Player(playername, basehealth, basemana, 10, 1, gold, playerspecial);
+            Object Player = CreacureGenerate.PlayerCreate();
             Console.WriteLine("Характеристики персонажа");
-            Console.WriteLine($"Имя героя: {player.name}");
-            Console.WriteLine($"Класс: {player.special}");
-            Console.WriteLine($"Уровень: {player.Level}");
-            Console.WriteLine($"Здоровье: {player.Health}");
-            Console.WriteLine($"Мана: {player.Mana}");
-            Console.WriteLine($"Золото: {player.Gold}");
-            Console.WriteLine($"Опыт: {player.Exp}");
+            Console.WriteLine($"Имя героя: {Player.name}");
+            Console.WriteLine($"Класс: {Player.special}");
+            Console.WriteLine($"Уровень: {Player.Level}");
+            Console.WriteLine($"Здоровье: {Player.Health}");
+            Console.WriteLine($"Мана: {Player.Mana}");
+            Console.WriteLine($"Золото: {Player.Gold}");
+            Console.WriteLine($"Опыт: {Player.Exp}");
             Console.WriteLine("Оракул: Твои приключения начинаются!");
             Console.ReadKey();
         }
