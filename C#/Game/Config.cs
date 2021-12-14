2a224987d5793468c7f89f037abaab3f6277356c
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-14 17:35:00 +0300

    Дополнил конфиг

--- C#/Game/Config.cs
+++ C#/Game/Config.cs
@@ -5,22 +5,31 @@ namespace GameConfig
 
         static private int mapSize = 100;
         static private int gameObjectsLimit = 100;
-        static private string difficulty = "Normal";
+        static private int creatureMaxLevel = 100;
+        static private string gameDifficulty = "Normal";
 
         public static int MapSize
         {
             get => mapSize;
             set => mapSize = value;
         }
+
         public static int GameObjectsLimit
         {
             get => gameObjectsLimit;
             set => gameObjectsLimit = value;
         }
-        public static string Difficulty
+
+        public static int CreatureMaxLevel
         {
-            get => difficulty;
-            set => difficulty = value;
+            get => creatureMaxLevel;
+            set => creatureMaxLevel = value;
+        }
+
+        public static string GameDifficulty
+        {
+            get => gameDifficulty;
+            set => gameDifficulty = value;
         }
 
     }
