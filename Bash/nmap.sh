0f74407ff89108976a6eefe7a33a42e1ab2e3008
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-14 14:01:00 +0300

    Update and rename Bash/Work/nmap.sh to Bash/nmap.sh

--- Bash/nmap.sh
+++ Bash/nmap.sh
@@ -8,7 +8,9 @@ for((i=1;i<$step+1;i++))
 do
     ip=$(cat hosts | sed -n "$i"p)
     echo "Start scanning ports - "$ip
+    
     result=$(nmap -sS -p 0-65535 -T5 -O -vvv -n -PS80,22 --reason $ip | sort | grep Discovered | sed 's/Discovered open port //g' | sed 's/\/tcp on '$ip'//g')
+    
     echo "Print result in file ./ports/"$ip
     touch ./ports/$ip
     echo $result > ./ports/$ip
