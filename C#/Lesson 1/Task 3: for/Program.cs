00b83274be608aa2165e3867ec46adea70af10c3
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-14 14:15:29 +0300

    Update Program.cs

--- C#/Lesson 1/Task 3: for/Program.cs
+++ C#/Lesson 1/Task 3: for/Program.cs
@@ -9,20 +9,29 @@ using System;
 
 namespace ProcentCalc
 {
+    
     class Program
     {
+        
         static void Main(string[] args)
         {
+            
             Console.Write("Введите сумму вклада: ");
             decimal Contribution = Convert.ToDecimal(Console.ReadLine());
+            
             Console.Write("Введите кол-во месяцве: ");
             int Month = Convert.ToInt32(Console.ReadLine());
+            
             for (int i = 0; i < Month; i++)
             {
                 Contribution += Contribution * 0.07M;
                 Console.WriteLine($"Ваш вклад за {i+1} месяц составляет: {Contribution}");
             }
+            
             Console.ReadKey();
+            
         }
+        
     }
+    
 }
