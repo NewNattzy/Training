d54feed5a7e8cb1fc2abbddac7a5dfe3f0628b82
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-14 14:14:51 +0300

    Update Program.cs

--- C#/Lesson 1/Task 2: switch-case/Program.cs
+++ C#/Lesson 1/Task 2: switch-case/Program.cs
@@ -8,32 +8,45 @@ using System;
 
 namespace SimpleCalc
 {
+    
     class Program
     {
+        
         static void Main(string[] args)
         {
+            
             Console.Write("Введите X: ");
             int x = Convert.ToInt32(Console.ReadLine());
+            
             Console.Write("Введите Y: ");
             int y = Convert.ToInt32(Console.ReadLine());
+            
             Console.WriteLine("Выберите тип операции: 1.Сложение, 2.Вычитание, 3.Умножение");
             byte operation = Convert.ToByte(Console.ReadLine());
+            
             switch (operation)
             {
                 case 1:
                     Console.WriteLine(x + y);
                     break;
+                    
                 case 2:
                     Console.WriteLine(x - y);
                     break;
+                    
                 case 3:
                     Console.WriteLine(x * y);
                     break;
+                    
                 default:
                     Console.WriteLine("Неизвестная операция");
                     break;
             }
+            
             Console.ReadKey();
+            
         }
+        
     }
+    
 }
