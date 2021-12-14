0f2ab713c720784057e13e43eedb3debfb2db1a4
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-14 14:02:07 +0300

    Update and rename PowerShell/Work/CheckDynamicIP.ps1 to PowerShell/CheckDynamicIP.ps1

--- PowerShell/CheckDynamicIP.ps1
+++ PowerShell/CheckDynamicIP.ps1
@@ -1,8 +1,8 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('cp866')
-Clear-Host
 
 $check_static_ip = netsh interface ip show address name="Ethernet" | out-String
 $check_static_ip = $check_static_ip.Contains('Да')
+
 if ($check_static_ip -eq 'True') {
     Write-Output "IP динамический"
 }
