4e12d6de188d6514a2d6834db2994f3a7795dac7
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-15 18:50:32 +0300

    Create Wilderness.cs

---
+++ C#/Game/GameObjects/Wilderness.cs
@@ -0,0 +1,14 @@
+namespace GameObjects
+{
+    public class Wilderness : Location
+    {
+
+        public Wilderness(string name, string type, string landscape, int danger, int population)
+            : base(name, type, landscape, danger, population)
+        {
+
+        }
+
+    }
+
+}
