26bece4333141e2e0d6fa11a90320c9056e91db5
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-15 18:49:49 +0300

    Create City.cs

---
+++ C#/Game/GameObjects/City.cs
@@ -0,0 +1,14 @@
+namespace GameObjects
+{
+    public class City : Location
+    {
+
+        public City(string name, string type, string landscape, int danger, int population)
+            : base (name, type, landscape, danger, population)
+        {
+
+        }
+
+    }
+
+}
