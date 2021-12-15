052e3017b9c3252a9e9bbe06e0dea9cc2ad73819
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-15 18:50:08 +0300

    Create Village.cs

---
+++ C#/Game/GameObjects/Village.cs
@@ -0,0 +1,14 @@
+namespace GameObjects
+{
+    public class Village : Location
+    {
+
+        public Village(string name, string type, string landscape, int danger, int population)
+            : base(name, type, landscape, danger, population)
+        {
+
+        }
+
+    }
+
+}
