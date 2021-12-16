310c8e893c99e30cd99ba4e9fee82311d27ba451
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-16 19:50:49 +0300

    Update City.cs

--- C#/Game/GameObjects/City.cs
+++ C#/Game/GameObjects/City.cs
@@ -1,12 +1,57 @@
+using GameConfig;
+using Interfaces;
+
 namespace GameObjects
 {
-    public class City : Location
+    public class City : Location, ILocation
     {
+
+        private static int maxPopulation = Config.BaseLocationPopulation * 40;
 
         public City(string name, string type, string landscape, int danger, int population)
             : base (name, type, landscape, danger, population)
         {
 
+        }
+
+        public override void Sieged()
+        {
+            Status = "Осажден";
+
+            if (Population % 2 == 0 && Population != 0)
+                Population /= 2;
+            else if (Population < 99)
+                Destroyed();
+            else
+            {
+                Population++;
+                Population /= 2;
+            }
+
+            Danger = 8;
+        }
+
+        public override void Cursed()
+        {
+            Status = "Проклят";
+            Population -= 50 * Danger;
+
+            if (Population <= 0)
+                Destroyed();
+            if (Danger < 10)
+                Danger++;
+        }
+
+        public override void Improved()
+        {
+            if (Population < maxPopulation)
+                Population += 250;
+            else if (Population > maxPopulation)
+            { 
+                Population = maxPopulation;
+                Status = "Процветает";
+                Danger = 0;
+            }  
         }
 
     }
