42ae57cb165a388bc1486edf2a6f65486e013fd8
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-16 18:22:58 +0300

    Update ILocation.cs

--- C#/Game/Interfaces/ILocation.cs
+++ C#/Game/Interfaces/ILocation.cs
@@ -3,6 +3,22 @@ namespace Interfaces
     public interface ILocation
     {
 
+        public char MapTyle { get; set; }
+
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+        public string Landscape { get; set; }
+
+        public int Danger { get; set; }
+        public int Population { get; set; }
+
+        public void Destroy();
+
+        public void Siege();
+
+        public void Restored();
+
     }
 
 }
