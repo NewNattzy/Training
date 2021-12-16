921bca574c8dd2b0dbdafcbb4336d0590ec5aed8
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-12-16 21:17:34 +0300

    Update Creature.cs

--- C#/Game/GameObjects/Creature.cs
+++ C#/Game/GameObjects/Creature.cs
@@ -4,8 +4,7 @@ namespace GameObjects
     public abstract class Creature
     {
 
-        public string Name;
-
+        private protected string name;
         private protected int health;
         private protected int mana;
 
@@ -15,42 +14,43 @@ namespace GameObjects
 
         public Creature(string name, int basehealth, int basemana, int damage, int level, int gold)
         {
-            Name = name;
+            this.name = name;
             this.level = level;
-            health = basehealth * level;
-            mana = basemana * level;
+            this.health = basehealth * level;
+            this.mana = basemana * level;
             this.damage = damage;
             this.gold = gold;
         }
 
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
         public int Damage
         {
-            get { return damage; }
-            set { damage = value; }
+            get => damage;
+            set => damage = value;
         }
 
         public int Level
         {
-            get { return level; }
-            set { level = value; }
+            get => level;
+            set => level = value;
         }
 
         public int Gold
         {
-            get { return gold; }
-            set { gold = value; }
+            get => gold;
+            set => gold = value;
         }
 
         public abstract int Health { get; set; }
 
         public abstract int Mana { get; set; }
 
-        public abstract void Talk(string words);
-
-        //public Enemy this [int index]
-        //{
-        //    get { return }
-        //}
+        public abstract void Talk(string message);
 
     }
 
